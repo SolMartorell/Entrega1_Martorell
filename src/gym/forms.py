cde5272b72f007927b2bcff0274eca06f6eb6319
Sol Martorell <msolmartorell@gmail.com>
2022-08-09 22:24:13 -0300

    Incorporacion de formularios de busqueda

--- src/gym/forms.py
+++ src/gym/forms.py
@@ -1,2 +1,10 @@
-from django.forms import Form, CharField
+from django.forms import Form, CharField, IntegerField, EmailField, BooleanField
 
+class BusquedaActividades(Form):
+    nombre_actividad = CharField(max_length=30)
+
+class BusquedaSocios(Form):
+    apellido_socio = CharField(max_length=30)
+
+class BusquedaPlanes(Form):
+    cantidad_clases_plan = IntegerField()
--- src/gym/views.py
+++ src/gym/views.py
@@ -1,7 +1,8 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from gym.models import Actividades, Socios, Planes
+from gym.forms import BusquedaActividades, BusquedaSocios, BusquedaPlanes
+
 
 
 
@@ -10,13 +11,55 @@ def inicio (request):
 
 def actividades (request):
     actividades = Actividades.objects.all()
-    return render(request, "gym/actividades.html", {"actividades": actividades})
 
+    if request.GET.get("nombre_actividad"):
+        
+        formulario = BusquedaActividades(request.GET)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            actividades = Actividades.objects.filter(nombre__icontains = data["nombre_actividad"])
+        return render(request, "gym/actividades.html", {"actividades": actividades})
+    
+    else:
+        formulario = BusquedaActividades()
+        return render(request, "gym/actividades.html", {"actividades": actividades, "formulario": formulario})
+
+
+
+    
 def socios (request):
     socios = Socios.objects.all()
-    return render(request, "gym/socios.html", {"socios": socios})
+
+    if request.GET.get("apellido_socio"):
+        
+        formulario = BusquedaSocios(request.GET)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            socios = Socios.objects.filter(apellido__icontains = data["apellido_socio"])
+        return render(request, "gym/socios.html", {"socios": socios})
+    
+    else:
+        formulario = BusquedaSocios()
+        return render(request, "gym/socios.html", {"socios": socios, "formulario": formulario})
+
+
 
 def planes (request):
     planes = Planes.objects.all()
-    return render(request, "gym/planes.html", {"planes": planes})
+
+    if request.GET.get("cantidad_clases_plan"):
+    
+        formulario = BusquedaPlanes(request.GET)
+    
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            planes = Planes.objects.filter(cantidad_clases__icontains = data["cantidad_clases_plan"])
+        return render(request, "gym/planes.html", {"planes": planes})
+
+    else:
+        formulario = BusquedaPlanes()
+        return render(request, "gym/planes.html", {"planes": planes, "formulario": formulario})
+    
     
